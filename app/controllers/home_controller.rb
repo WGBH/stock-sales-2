01ef19c37db4db35ac29083cb7d2fd1558d1fdef
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-20 18:28:08 -0400

    Collections in data structure.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,15 @@
 class HomeController < ApplicationController
+  def index
+    @collections = [
+      {name: 'NOVA', href: '/collections/nova', img_src: 'http://placehold.it/269x151'},
+      {name: 'Frontline', href: '/collections/frontline', img_src: 'http://placehold.it/269x151'},
+      {name: 'American Experience', href: '/collections/amex', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
+      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'}
+    ]
+  end
 end
