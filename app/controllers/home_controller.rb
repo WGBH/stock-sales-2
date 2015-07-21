c65756ac9da8b4c24f0b2c77cbf99c9d520f06b3
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-07-21 09:49:04 -0400

    More style tweaks.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,16 @@
 class HomeController < ApplicationController
   def index
+    img_src = 'http://placehold.it/272x152'
     @collections = [
-      {name: 'NOVA', href: '/collections/nova', img_src: 'http://placehold.it/269x151'},
-      {name: 'Frontline', href: '/collections/frontline', img_src: 'http://placehold.it/269x151'},
-      {name: 'American Experience', href: '/collections/amex', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'},
-      {name: 'TBD', href: '/collections/tbd', img_src: 'http://placehold.it/269x151'}
+      {name: 'NOVA', href: '/collections/nova', img_src: img_src},
+      {name: 'Frontline', href: '/collections/frontline', img_src: img_src},
+      {name: 'American Experience', href: '/collections/amex', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src},
+      {name: 'TBD', href: '/collections/tbd', img_src: img_src}
     ]
   end
 end
