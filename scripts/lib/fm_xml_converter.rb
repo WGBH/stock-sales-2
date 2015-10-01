555da3377599fd84cbf88ed113e04a67431fb4d5
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-10-01 12:16:55 -0400

    FM-XML -> JSON

--- scripts/lib/fm_xml_converter.rb
+++ scripts/lib/fm_xml_converter.rb
@@ -3,18 +3,28 @@ require 'nokogiri'
 require 'json'
 
 class FmXmlConverter
-  include Singleton
+  include Enumerable
   
-  def initialize
-    # ???
+  def initialize(xml)
+    @doc = Nokogiri::XML(xml).remove_namespaces!
   end
   
-  def convert_record(xml)
+  def each(&block)
+    @doc.xpath('/FMPDSORESULT/ROW').each do |record|
+      # Reparsing every subelement isn't efficient, 
+      # but it lets methods operate on strings, which
+      # makes testing easier.
+      block.call(self.class.convert_record(record.to_xml))
+    end
+  end
+  
+  def self.convert_record(xml)
     doc = Nokogiri::XML(xml)
     JSON.pretty_generate(
       Hash[ doc.xpath('/ROW/*').map { |el| 
         [el.name.downcase, el.text]
       } ]
     )
-  end  
+  end
+  
 end
--- spec/scripts/fm_xml_converter_spec.rb
+++ spec/scripts/fm_xml_converter_spec.rb
@@ -4,9 +4,16 @@ describe FmXmlConverter do
 
   it 'converts record' do
     xml = File.read('spec/fixtures/fm-export-record.xml')
-    json = FmXmlConverter.instance.convert_record(xml)
+    json = FmXmlConverter.convert_record(xml)
     json_expected = File.read('spec/fixtures/fm-export-record.expected.json')
     expect(json).to eq(json_expected)
+  end
+  
+  it 'converts results' do
+    xml = File.read('spec/fixtures/fm-export-results.xml')
+    array = FmXmlConverter.new(xml).map { |json| json }
+    json_expected = File.read('spec/fixtures/fm-export-record.expected.json')
+    expect(array.first).to eq(json_expected)
   end
   
 end
