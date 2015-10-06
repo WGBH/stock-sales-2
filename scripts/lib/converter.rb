1f4a404bd017dc6f92920ce22934892dd4981b4d
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-10-06 18:07:51 -0400

    Check point. Not super confident that this is doing the right thing.

--- scripts/lib/converter.rb
+++ scripts/lib/converter.rb
@@ -1,6 +1,7 @@
 require 'singleton'
 require 'nokogiri'
 require 'json'
+require 'sony-ci-api'
 require_relative '../../app/models/asset'
 
 class Converter
@@ -8,6 +9,23 @@ class Converter
   
   def initialize(xml)
     @doc = Nokogiri::XML(xml).remove_namespaces!
+    
+    @thumbs = {}
+    ci = SonyCiAdmin.new(credentials_path: 'config/ci.yml')
+    ci_ids = @doc.xpath('/FMPDSORESULT/ROW/Ci_ID').map(&:text).reject(&:empty?)
+    while !ci_ids.empty?
+      group = ci_ids.shift(500)
+      puts "#{ci_ids.count} Ci IDs still need details"
+      details = ci.multi_details(group, ['thumbnails'])
+      @thumbs.merge(Hash[
+        details['items'].map { |item| [
+          item['id'], 
+          item['thumbnails'].select { |thumbnail| 
+            thumbnail['type'] == 'small' 
+          }.first['location']
+        ] } 
+      ] )
+    end
   end
   
   def each(&block)
@@ -15,16 +33,19 @@ class Converter
       # Reparsing every subelement isn't efficient, 
       # but it lets methods operate on strings, which
       # makes testing easier.
-      block.call(self.class.to_asset(record.to_xml))
+      block.call(to_asset(record.to_xml))
     end
   end
   
-  def self.to_asset(xml)
+  def to_asset(xml)
     doc = Nokogiri::XML(xml)
     Asset.new(
-      Hash[ doc.xpath('/ROW/*').map { |el| 
-        [el.name.downcase, el.text]
-      } ]
+      begin
+        hash = Hash[ doc.xpath('/ROW/*').map { |el| 
+          [el.name.downcase, el.text]
+        } ]
+        hash.merge({'thumb_src' => @thumbs[hash['ci_id']]})
+      end
     )
   end
   
