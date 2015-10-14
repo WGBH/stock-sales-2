848fd739b977cfa76f39eb65cfc367ca39a8a2d1
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-10-14 15:04:07 -0400

    Fix fixtures and tests.

--- spec/scripts/converter_spec.rb
+++ spec/scripts/converter_spec.rb
@@ -4,7 +4,7 @@ describe Converter do
   
   it 'converts results' do
     xml = File.read('spec/fixtures/fm-export-results.xml')
-    array = Converter.new(xml, 'spec/fixtures/thumb-src-cache.json').map { |asset| asset.to_json }
+    array = Converter.new(xml, 'spec/fixtures/stock-sales-cache.json').map { |asset| asset.to_json }
     json_expected = File.read('spec/fixtures/fm-export-record.expected.json')
     expect(array.first).to eq(json_expected)
   end
