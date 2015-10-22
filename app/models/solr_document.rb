7f2b3052d51dfd1baad18a93474c79d23494c136
Andrew Myers <afredmyers@gmail.com>
2015-10-22 11:22:25 -0400

    Refactors the catalog show view
    * Uses SolrDocument as the presenter instead of Asset.
    * Decouples field_list partial from its containing view.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,3 +1,23 @@
 class SolrDocument 
   include Blacklight::Solr::Document
+
+  attr_accessor :json
+
+  def json
+    @json ||= (JSON.parse(@_source[:json]) rescue {}).with_indifferent_access
+  end
+
+  def label_for(field)
+    # Assume labels are the same as field names, sans underscores and
+    # "clip_" prefix if it has one.
+    field.to_s.gsub('_',' ').gsub(/clip /i, '')
+  end
+
+  def value_of(field)
+    @_source[field] || json[field]
+  end
+
+  def labels_and_values(*fields)
+    fields.map{ |field| {label: label_for(field), value: value_of(field)} }.select{ |pair| !pair[:value].nil? && !pair[:value].empty? }
+  end
 end
