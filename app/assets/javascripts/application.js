c1752772eba3321786ed7115f67d1c9e08c30ca0
Andrew Myers <afredmyers@gmail.com>
2016-01-05 11:20:57 -0500

    Update application.js
    changes UA id

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,5 +23,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-60323667-1', 'auto');
+ga('create', 'UA-72013145-1', 'auto');
 ga('send', 'pageview');
