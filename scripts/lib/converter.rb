9a18e8ff86af302c3c2b7588fdfc388666c4f4d3
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-06-29 14:45:13 -0400

    Special handling for programs.

--- scripts/lib/converter.rb
+++ scripts/lib/converter.rb
@@ -32,29 +32,36 @@ class Converter
   THUMBNAILS = 'thumbnails'
   PROXIES = 'proxies'
   
+  @@logos = {
+    'NOVA' => 'https://s3.amazonaws.com/wgbhstocksales.org/content/collections/nova/nova-thumb_348x196.png',
+    'Frontline' => 'https://s3.amazonaws.com/wgbhstocksales.org/content/collections/frontline/frontline-thumb_348x196.png',
+    'American Experience' => 'https://s3.amazonaws.com/wgbhstocksales.org/content/collections/amex/amex-thumb_348x196.png'
+  }
+  
   def to_solr_record(fm_record_xml)
     SolrDocument.new({
-      json:
-        begin
+        json:
+          begin
           fm_record_doc = Nokogiri::XML(fm_record_xml)
           fm_record_hash = Hash[ fm_record_doc.xpath('/ROW/*').map { |el| 
-            [el.name.downcase, el.text.strip]
-          } ]
+              [el.name.downcase, el.text.strip]
+            } ]
           unless fm_record_hash['id']
             fail("No id in #{fm_record_xml}") 
           end
-          unless fm_record_hash['ci_id']
-            fail("No ci_id for FM <#{fm_record_hash['id']}>") 
-          end
-          unless @cache[fm_record_hash['ci_id']]
-            fail("No cache for Ci <#{fm_record_hash['ci_id']}> / FM <#{fm_record_hash['id']}>") 
+          thumb_src = if !fm_record_hash['ci_id'].empty?
+            fail("No cache for Ci <#{fm_record_hash['ci_id']}> / FM <#{fm_record_hash['id']}>") if !@cache[fm_record_hash['ci_id']]
+            @cache[fm_record_hash['ci_id']][THUMBNAILS].select { |thumbnail| 
+              thumbnail['type'] == 'small' 
+            }.first['location']
+          else
+            @@logos[fm_record_hash['series']]
           end
           fm_record_hash.merge({
-            'thumb_src' => @cache[fm_record_hash['ci_id']][THUMBNAILS].select { |thumbnail| 
-                  thumbnail['type'] == 'small' 
-                }.first['location']})
+              'thumb_src' => thumb_src
+            })
         end.to_json
-    })
+      })
   end
   
   def refresh_cache()
@@ -62,7 +69,7 @@ class Converter
     $LOG.info("Cache starting with #{@cache.count} entries")
     
     ci_ids_todo = @doc.xpath('/FMPDSORESULT/ROW/Ci_ID')
-                 .map(&:text).map(&:strip).reject(&:empty?) - @cache.keys
+    .map(&:text).map(&:strip).reject(&:empty?) - @cache.keys
     
     if !ci_ids_todo.empty?
       ci = SonyCiAdmin.new(credentials_path: 'config/ci.yml')
@@ -71,14 +78,14 @@ class Converter
         $LOG.info("#{ci_ids_todo.count} Ci IDs still need details")
         details = ci.multi_details(group, [THUMBNAILS, PROXIES])
         @cache.merge!(Hash[
-          details['items'].map { |item| [
-            item['id'], 
-            {
-              THUMBNAILS => item[THUMBNAILS],
-              PROXIES => item[PROXIES],
-            }
-          ] } 
-        ] )
+            details['items'].map { |item| [
+                item['id'], 
+                {
+                  THUMBNAILS => item[THUMBNAILS],
+                  PROXIES => item[PROXIES],
+                }
+              ] } 
+          ] )
         File.write(@cache_path, JSON.pretty_generate(@cache))
       end
     end
