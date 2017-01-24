44710a0061bcf51f149468048f5e57f9d8a9a3da
Jason Corum <jason.corum@gmail.com>
2017-01-24 10:21:41 -0500

    Changes name of Mailer test file to match Mailer
    * Includes removal of yml require in payments_js spec

---
+++ spec/mailers/stocksales_mailer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe StocksalesMailer, type: :mailer do
+
+  describe '#successful_transaction' do
+    before(:each) do
+      params = {"email"=>"test@test.com", "amount"=>"50", "description"=>"Tall Ships Footage", "action"=>"create", "controller"=>"payments"}
+      StocksalesMailer.successful_transaction(params).deliver
+    end
+
+    after(:each) do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it 'should be sent to specified email address' do
+      expect(ActionMailer::Base.deliveries.first.to.first).to eq('test@test.com')
+    end
+
+    it 'should be sent from specified email address' do
+      expect(ActionMailer::Base.deliveries.first.from.first).to eq('jason_corum@wgbh.org')
+    end
+
+    it 'should be sent with specified subject' do
+      expect(ActionMailer::Base.deliveries.first.subject).to eq('Thank You For your Purchase')
+    end
+  end
+end
