e84448032631041fbfb41ee10858a5d345766972
Jason Corum <jason.corum@gmail.com>
2017-01-25 09:35:07 -0500

    Removes reference to Amazon yml file for change to env vars

--- app/mailers/stocksales_mailer.rb
+++ app/mailers/stocksales_mailer.rb
@@ -1,9 +1,5 @@
 class StocksalesMailer < ActionMailer::Base
 
-  smtp_creds = YAML.load_file(Rails.root + 'config/amazon_smtp.yml')
-  SMTP_USER = smtp_creds['smtp_username']
-  SMTP_PASSWORD = smtp_creds['smtp_password']
-
   # NEED TO CHANGE PRIOR TO LAUNCH
   default from: "jason_corum@wgbh.org" # WgbhStocksales::EMAIL
 
