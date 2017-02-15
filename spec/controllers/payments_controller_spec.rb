63cc6a7ac87ec5b2aab8cc4df706d2adcb567d61
Jason Corum <jason.corum@gmail.com>
2017-02-15 10:44:48 -0500

    Tests which email is sent from payments_controller

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
-require 'pry'
 
 describe PaymentsController do
-
   let(:result) {
     "{\"Response\":{\"status\":\"Approved\",\"reference\":\"D2EHFF88e0\",\"message\":\"APPROVED 519519                 \",\"code\":\"519519\",\"cvvResult\":\"P\",\"avsResult\":\" \",\"riskCode\":\"00\",\"networkId\":\"10\",\"isPurchaseCard\":false,\"vaultResponse\":{\"status\":1,\"data\":\"67567508f6d241709df068adc59e6b76\"},\"orderNumber\":\"1\",\"transactionId\":\"ZmJlYzNiZjMwYzM3ZGJlNDYxZTYwMGEyMjE3YTdiYzY\",\"timestamp\":\"2017-02-14T15:15:08.535454-05:00\"},\"Hash\":\"pM/ br7N 9xczU 2WkH7xVwTEYFzLR0ps AnuG2fl7zOFY0Eij5X6e0kCMFxmsed1/0SOcMAFkSLXLUAjaBvUA==\"}"
   }
@@ -11,15 +9,42 @@ describe PaymentsController do
     "{\"Response\":{\"status\":\"Approved\",\"reference\":\"D2EHFF88e0\",\"message\":\"APPROVED 519519                 \",\"code\":\"519519\",\"cvvResult\":\"P\",\"avsResult\":\" \",\"riskCode\":\"00\",\"networkId\":\"10\",\"isPurchaseCard\":false,\"vaultResponse\":{\"status\":1,\"data\":\"67567508f6d241709df068adc59e6b76\"},\"orderNumber\":\"1\",\"transactionId\":\"ZmJlYzNiZjMwYzM3ZGJlNDYxZTYwMGEyMjE3YTdiYzY\",\"timestamp\":\"2017-02-14T15:15:08.535454-05:00\"},\"Hash\":\"pM/ br7N 9xczU 2WkH7xVwTEYFzLR0ps AnuG2fl7zOFY0Eij5X6e0kCMFxmsed1/0SOcMAFkSLXLUAjaBvUA==BAD==\"}"
   }
 
+  let(:good_post) {
+    post :create,
+    email: 'jason_corum@wgbh.org',
+    amount: "50.00",
+    description: "Tall Ships Footage",
+    order_number: '1234',
+    result: result
+  }
+
+  let(:bad_post) {
+    post :create,
+    email: 'jason_corum@wgbh.org',
+    amount: "50.00",
+    description: "Tall Ships Footage",
+    order_number: '1234',
+    result: bad_result
+  }
+
+  let(:good_subject) { 'Thank You For your Purchase' }
+
+  let(:bad_subject) { 'Suspect Transaction' }
+
   describe 'POST index' do
-    it 'sends an email if result hash is good' do
-      expect { post :create, email: 'jason_corum@wgbh.org', amount: "50.00", description: "Tall Ships Footage", order_number: '1234', result: result }
-      .to change { ActionMailer::Base.deliveries.count }.by(1)
+
+    before(:each) do
+      ActionMailer::Base.deliveries.clear
     end
 
-    it 'sends an email if result hash is bad' do
-      expect { post :create, email: 'jason_corum@wgbh.org', amount: "50.00", description: "Tall Ships Footage", order_number: '1234', result: bad_result }
-      .to change { ActionMailer::Base.deliveries.count }.by(1)
+    it 'sends success email if result hash is good' do
+      expect{ good_post }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+      expect(ActionMailer::Base.deliveries.first.subject).to eq(good_subject)
+    end
+
+    it 'sends suspect transaction email if result hash is bad' do
+      expect{ bad_post }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+      expect(ActionMailer::Base.deliveries.first.subject).to eq(bad_subject)
     end
   end
 end
